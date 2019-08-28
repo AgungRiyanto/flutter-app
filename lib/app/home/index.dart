3bbef3323bef2b71ca3b052d6b2111783c463a7a
agung reyanto <agungreyanto@marlinbooking.id>
2019-08-28 15:42:33 +0700

    update

--- lib/app/home/index.dart
+++ lib/app/home/index.dart
@@ -10,8 +10,21 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(
-        child: Text('home'),
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+            right: 20.0,
+            bottom: 20.0,
+            child: Container(
+              padding: EdgeInsets.all(13),
+              child: Icon(Icons.add, color: Colors.white,),
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(255, 2, 102, 1),
+                borderRadius: BorderRadius.all(Radius.circular(100))
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
