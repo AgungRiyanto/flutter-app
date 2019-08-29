1e9f5fd9634a596a4c0a36e578e7f5347de3a9c8
agung reyanto <agungreyanto@marlinbooking.id>
2019-08-29 17:57:10 +0700

    update

--- lib/app/index.dart
+++ lib/app/index.dart
@@ -64,9 +64,15 @@ class _AppState extends State<App> {
                     ),
                     Expanded(
                       flex: 1,
-                      child: Container(
-                        margin: EdgeInsets.only(left: 7),
-                        child: Icon(Icons.add, color: variable.primary),
+                      child: InkWell(
+                        splashColor: Theme.of(context).splashColor,
+                        highlightColor: Theme.of(context).highlightColor,
+                        onTap: () {
+                        },
+                        child: Container(
+                          margin: EdgeInsets.only(left: 7),
+                          child: Icon(Icons.add, color: variable.primary),
+                        ),
                       ),
                     ),
                   ],
